85e81b8db4cae3985ec71d64e64dc2134cc4edea
nikneem <ekeilholz+git@outlook.com>
2022-05-30 16:02:04 +0200

    Adding a password value object (#6)
    Co-authored-by: Eduard <e.keilholz@4dotnet.nl>

---
+++ src/HexMaster.DomainDrivenDesign/ValueObjects/Password.cs
@@ -0,0 +1,28 @@
+﻿using HexMaster.DomainDrivenDesign.Helpers;
+
+namespace HexMaster.DomainDrivenDesign.ValueObjects
+{
+    public sealed class Password
+    {
+
+        public string EncryptedPassword { get; }
+        public string Secret { get; }
+
+        public bool Verify(string plainTextPassword)
+        {
+            return PasswordHasherHelper.Verify(plainTextPassword, EncryptedPassword, Secret);
+        }
+
+        public Password(string plainTextPassword)
+        {
+            Secret = PasswordHasherHelper.GenerateSalt();
+            EncryptedPassword = plainTextPassword.Hash(Secret);
+        }
+
+        public Password(string encryptedPassword, string secret)
+        {
+            EncryptedPassword = encryptedPassword;
+            Secret = secret;
+        }
+    }
+}
