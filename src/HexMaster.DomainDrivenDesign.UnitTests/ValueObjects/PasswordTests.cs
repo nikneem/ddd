f8c08b7f7efcd0e43bb1429a9c8882336d4be7b3
Eduard <e.keilholz@4dotnet.nl>
2022-05-30 16:00:09 +0200

    Adding a password value object

---
+++ src/HexMaster.DomainDrivenDesign.UnitTests/ValueObjects/PasswordTests.cs
@@ -0,0 +1,24 @@
+﻿using HexMaster.DomainDrivenDesign.ValueObjects;
+using Xunit;
+
+namespace HexMaster.DomainDrivenDesign.UnitTests.ValueObjects;
+
+public class PasswordTests
+{
+    [Fact]
+    public void WhenPasswordIsGenerated_ItVerifiesSuccesfully()
+    {
+        var plainTextPassword = "PlainTextPassword";
+        var passwordObject = new Password(plainTextPassword);
+        Assert.True(passwordObject.Verify(plainTextPassword));
+    }
+
+    [Fact]
+    public void WhenPasswordReCreated_ItStillVerifies()
+    {
+        var plainTextPassword = "PlainTextPassword";
+        var passwordObject = new Password(plainTextPassword);
+        var copiedObject = new Password(passwordObject.EncryptedPassword, passwordObject.Secret);
+        Assert.True(copiedObject.Verify(plainTextPassword));
+    }
+}
---
+++ src/HexMaster.DomainDrivenDesign/Helpers/PasswordHasherHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace HexMaster.DomainDrivenDesign.Helpers
+{
+    public static class PasswordHasherHelper
+    {
+
+        private const int SaltSize = 16;
+
+        public static string Hash(this string password, string salt, int iterations = 10000)
+        {
+            var s = Convert.FromBase64String(salt);
+            if (s.Length != SaltSize)
+            {
+                throw new Exception("Invalid salt size");
+            }
+
+            return Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: s,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 100000,
+                numBytesRequested: 256 / 8));
+        }
+
+        public static string GenerateSalt()
+        {
+            byte[] salt = new byte[SaltSize];
+            RandomNumberGenerator.Fill(salt);
+            return Convert.ToBase64String(salt);
+        }
+
+
+        public static bool Verify(string password, string hashedPassword, string salt)
+        {
+            var hash = Hash(password, salt);
+            return hashedPassword == hash;
+        }
+    }
+}
