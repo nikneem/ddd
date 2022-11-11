64cbd12d4d07897a46e0bd627253da12631e5361
Eduard <e.keilholz@4dotnet.nl>
2022-11-11 13:33:44 +0100

    Support for .net 7

--- src/HexMaster.DomainDrivenDesign/DomainNotificationBase.cs
+++ src/HexMaster.DomainDrivenDesign/DomainNotificationBase.cs
@@ -1,13 +1,15 @@
 ﻿using HexMaster.DomainDrivenDesign.Abstractions.Events;
 
-namespace HexMaster.DomainDrivenDesign;
-
-public class DomainNotificationBase <T> : IDomainEventNotification<T>  where T : IDomainEvent
+namespace HexMaster.DomainDrivenDesign
 {
-    public T DomainEvent { get; }
 
-    public DomainNotificationBase(T domainEvent)
+    public class DomainNotificationBase<T> : IDomainEventNotification<T> where T : IDomainEvent
     {
-        DomainEvent = domainEvent;
+        public T DomainEvent { get; }
+
+        public DomainNotificationBase(T domainEvent)
+        {
+            DomainEvent = domainEvent;
+        }
     }
 }
