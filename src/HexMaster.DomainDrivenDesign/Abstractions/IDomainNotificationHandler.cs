f3a38b32a3bffa91425858aea8b8b27aacd3f636
Eduard Keilholz <e.keilholz@4dotnet.nl>
2025-02-05 10:43:32 +0100

    Adding a handler interface

---
+++ src/HexMaster.DomainDrivenDesign/Abstractions/IDomainNotificationHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace HexMaster.DomainDrivenDesign.Abstractions;
+
+public interface IDomainNotificationHandler
+{
+    Task Handle(IDomainNotification domainEvent, CancellationToken cancellationToken = default);
+}
