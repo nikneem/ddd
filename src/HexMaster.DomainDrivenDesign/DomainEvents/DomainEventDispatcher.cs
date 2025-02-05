b1e40ab9be9d7f61853a03e7f5e34409a1add0c3
Eduard Keilholz <e.keilholz@4dotnet.nl>
2025-02-05 15:00:18 +0100

    Add logging for event handler

--- src/HexMaster.DomainDrivenDesign/DomainEvents/DomainEventDispatcher.cs
+++ src/HexMaster.DomainDrivenDesign/DomainEvents/DomainEventDispatcher.cs
@@ -1,23 +1,37 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using System;
+using System.Diagnostics;
+using System.Linq;
 using HexMaster.DomainDrivenDesign.Abstractions;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace HexMaster.DomainDrivenDesign.DomainEvents;
 
-public class DomainEventDispatcher(IServiceProvider serviceProvider) : IDomainEventDispatcher
+public class DomainEventDispatcher(IServiceProvider serviceProvider, ILogger<DomainEventDispatcher> logger) : IDomainEventDispatcher
 {
     public async Task Dispatch(IDomainEvent domainEvent)
     {
-
         var handlerType = typeof(IDomainEventHandler<>).MakeGenericType(domainEvent.GetType());
-        var handlers = serviceProvider.GetServices(handlerType);
+        logger.LogInformation($"Dispatching event of type {domainEvent.GetType().Name} to handlers of type {handlerType.Name}");
+        var handlers = serviceProvider.GetServices(handlerType).ToList();
+        logger.LogInformation("Dispatching domain event to {count} handlers", handlers.Count);
         foreach (var handler in handlers)
         {
             if (handler is IDomainEventHandler domainEventHandler)
             {
-                await domainEventHandler.Handle(domainEvent);
+                var sw = Stopwatch.StartNew();
+                try
+                {
+                    logger.LogInformation("Executing handler for domain event {type}", domainEvent.GetType().Name);
+                    await domainEventHandler.Handle(domainEvent);
+                    logger.LogInformation("Handler executed in {ms} milliseconds", sw.ElapsedMilliseconds);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "Error executing handler for domain event {type}", domainEvent.GetType().Name);
+                }
             }
         }
     }
