c101a34d7008a1324d429bd4513517d81093fe30
nikneem <ekeilholz+git@outlook.com>
2025-10-06 12:47:39 +0200

    Update NuGet packages and enhance date range comparisons
    Updated project files to use newer versions of NuGet packages, including `Microsoft.NET.Test.Sdk`, `xunit`, and `coverlet.collector`.
    
    Modified `DateRange` and `OptionalDateRange` classes to implement `IComparable` interfaces, allowing for comparison of instances. Added methods for equality and ordering, along with XML documentation for clarity.

--- src/HexMaster.DomainDrivenDesign/ValueObjects/DateRange.cs
+++ src/HexMaster.DomainDrivenDesign/ValueObjects/DateRange.cs
@@ -3,7 +3,7 @@ using HexMaster.DomainDrivenDesign.Exceptions;
 
 namespace HexMaster.DomainDrivenDesign.ValueObjects;
 
-public class DateRange
+public class DateRange : IComparable<DateRange>
 {
     public DateTimeOffset StartDate { get; }
     public DateTimeOffset EndDate { get; }
@@ -17,5 +17,37 @@ public class DateRange
         }
         StartDate = start;
         EndDate = end;
+    }
+
+    /// <summary>
+    /// Compares this date range with another instance and returns true if the date values are different, false if they are the same.
+    /// </summary>
+    /// <param name="other">The other DateRange instance to compare with</param>
+    /// <returns>True if the date ranges are different, false if they are the same</returns>
+    public bool Compare(DateRange other)
+    {
+        if (other == null)
+            return true;
+
+        return StartDate != other.StartDate || EndDate != other.EndDate;
+    }
+
+    /// <summary>
+    /// Compares the current instance with another DateRange and returns an integer that indicates whether the current instance precedes, follows, or occurs in the same position in the sort order as the other object.
+    /// </summary>
+    /// <param name="other">The DateRange to compare with this instance</param>
+    /// <returns>A value that indicates the relative order of the objects being compared</returns>
+    public int CompareTo(DateRange other)
+    {
+        if (other == null)
+            return 1;
+
+        // First compare by StartDate
+        var startComparison = StartDate.CompareTo(other.StartDate);
+        if (startComparison != 0)
+            return startComparison;
+
+        // If StartDate is equal, compare by EndDate
+        return EndDate.CompareTo(other.EndDate);
     }
 }
--- src/HexMaster.DomainDrivenDesign/ValueObjects/OptionalDateRange.cs
+++ src/HexMaster.DomainDrivenDesign/ValueObjects/OptionalDateRange.cs
@@ -3,7 +3,7 @@ using HexMaster.DomainDrivenDesign.Exceptions;
 
 namespace HexMaster.DomainDrivenDesign.ValueObjects;
 
-public class OptionalDateRange
+public class OptionalDateRange : IComparable<OptionalDateRange>
 {
     public DateTimeOffset StartDate { get; } 
     public DateTimeOffset? EndDate { get; } 
@@ -22,5 +22,43 @@ public class OptionalDateRange
         EndDate = end;
     }
 
+    /// <summary>
+    /// Compares this optional date range with another instance and returns true if the date values are different, false if they are the same.
+    /// </summary>
+    /// <param name="other">The other OptionalDateRange instance to compare with</param>
+    /// <returns>True if the date ranges are different, false if they are the same</returns>
+    public bool Compare(OptionalDateRange other)
+    {
+        if (other == null)
+            return true;
+
+        return StartDate != other.StartDate || EndDate != other.EndDate;
+    }
+
+    /// <summary>
+    /// Compares the current instance with another OptionalDateRange and returns an integer that indicates whether the current instance precedes, follows, or occurs in the same position in the sort order as the other object.
+    /// </summary>
+    /// <param name="other">The OptionalDateRange to compare with this instance</param>
+    /// <returns>A value that indicates the relative order of the objects being compared</returns>
+    public int CompareTo(OptionalDateRange other)
+    {
+        if (other == null)
+            return 1;
+
+        // First compare by StartDate
+        var startComparison = StartDate.CompareTo(other.StartDate);
+        if (startComparison != 0)
+            return startComparison;
+
+        // If StartDate is equal, compare by EndDate
+        // Handle nullable EndDate comparison
+        if (EndDate == null && other.EndDate == null)
+            return 0;
+        if (EndDate == null)
+            return -1; // null EndDate is considered "less than" a value
+        if (other.EndDate == null)
+            return 1;
+
+        return EndDate.Value.CompareTo(other.EndDate.Value);
+    }
 }
-    
